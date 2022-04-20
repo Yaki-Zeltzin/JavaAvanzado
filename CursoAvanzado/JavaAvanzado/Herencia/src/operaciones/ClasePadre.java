4e0a1d39797ea42507e15d8fb3cfe5374b02ae1c
Zeltzin Romero <zeltzin.rom96@gmail.com>
2022-04-20 11:47:23 -0500

    Ejercicio con Herencia | Suma y Resta

--- CursoAvanzado/JavaAvanzado/Herencia/src/operaciones/ClasePadre.java
+++ CursoAvanzado/JavaAvanzado/Herencia/src/operaciones/ClasePadre.java
@@ -7,14 +7,14 @@ public class ClasePadre {
     Scanner entrada = new Scanner(System.in);
 
     public void PedirDatos(){
-        System.out.print("Dame el primer valor: ");
+        System.out.println("Dame el primer valor: ");
         valorUno = entrada.nextInt();
 
-        System.out.print("Dame el segundo valor: ");
+        System.out.println("Dame el segundo valor: ");
         valorDos = entrada.nextInt();
     }
 
     public void MostrarResultado(){
-        System.out.print(resultado);
+        System.out.println(resultado);
     }
 }
--- CursoAvanzado/JavaAvanzado/Herencia/src/perro/Mateo.java
+++
@@ -1,7 +0,0 @@
-package perro;
-
-public class Mateo extends Perro{
-    public void Presentar(){
-        presentar = name + raza;
-    }
-}
